b2a5d779f3ad22fd16544634992981acd86a8ce9
duanyiting2018 <1036179833@qq.com>
2018-11-16 20:52:49 +0800

    rgb

---
+++ 16-4.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
+import pygame
+pygame.init()
+screen=pygame.display.set_mode([640,480])
+screen.fill([200,205,55])
+pygame.draw.circle(screen,[255,245,250],[320,240],50,50)
+pygame.display.flip()
+running=True
+while running:
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+            running=False
+pygame.quit()
