7887525c62924435ea9fdc5e1d1ea5d1f6215325
duanyiting2018 <1036179833@qq.com>
2019-03-16 21:35:45 +0800

    match_man

--- learning_python/match_man.py
+++ learning_python/match_man.py
@@ -28,5 +28,54 @@ class game:
                               sprites.move()
                   self.tk.update()
                   time.sleep(0.03)
+class coords:
+      def __init__(self,x1=0,y1=0,x2=0,y2=0):
+            self.x1=x1
+            self.y1=y1
+            self.x2=x2
+            self.y2=y2
+      def w_x(co1,co2):
+            if co1.x1>co2.x1 and co1.x1<co2.x2:
+                  return True
+            elif co1.x2>co2.x1 and co1.x2<co2.x2:
+                  return True
+            elif co2.x1>co1.x1 and co2.x1<co1.x2:
+                  return True
+            elif co2.x2>co1.x1 and co2.x2<co1.x1:
+                  return True
+            else:
+                  return False
+      def w_y(co1,co2):
+            if co1.y1>co2.y1 and co1.y1<co2.y2:
+                  return True
+            elif co1.y2>co2.y1 and co1.y2<co2.y2:
+                  return True
+            elif co2.y1>co1.y1 and co2.y1<co1.y2:
+                  return True
+            elif co2.y2>co1.y1 and co2.y2<co1.y1:
+                  return True
+            else:
+                  return False
+      def c_l(co1,co2):
+            if w_y(co1,co2):
+                  if co1.x1<=co2.x2 and co1.x1>=co2.x1:
+                        return True
+            return False
+      def c_r(co1,co2):
+            if w_y(co1,co2):
+                  if co1.x2<=co2.x1 and co1.x2>=co2.x2:
+                        return True
+            return False
+      def c_t(co1,co2):
+            if w_x(co1,co2):
+                  if co1.y1<=co2.y2 and co1.y1>=co2.y1:
+                        return True
+            return False
+      def c_b(y,co1,co2):
+            if w_x(co1,co2):
+                  y_c=co1.y2+y
+                  if y_c>=co2.x1 and co1.x1<=co2.x2:
+                        return True
+            return False
 g=game()
 g.main()
